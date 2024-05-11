422ada5291cbcc1b287304a55a4a7de4a353df52
Jose David <josesucullani@gmail.com>
2024-05-11 13:59:11 +0200

    Añadiendo Login Version 0.0.1

--- BlogVue/src/App.vue
+++ BlogVue/src/App.vue
@@ -8,8 +8,9 @@
     <router-link to="/about">About</router-link>
     <router-link to="/login">Login</router-link>
     <router-link to="/etiqueta">Etiqueta</router-link>
-    <router-view></router-view>
+    
   </div>
+  <router-view></router-view>
 </template>
 
 
--- BlogVue/src/views/LoginView.vue
+++ BlogVue/src/views/LoginView.vue
@@ -1,26 +1,41 @@
-<script setup>
-
-</script>
-
+<script setup></script>
 
 <template>
-    <form >
-        <div class="Login">
-            <input type="email" placeholder="Ingresa Email">
-            <input type="password" placeholder="Ingresa Contraseña">
-        </div>
-    </form>
+  <div class="Principal">
+    <div class="Img">
+      <div class="email"><input type="email" placeholder="Email ID" /></div>
+      <div class="password">
+        <input type="password" placeholder="Password" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.Formulario{
-    
+
+.Principal{
+    margin: 0px 0px 250px 400px;
 }
 
-.Login{
-    width: 250px;
-    height: 300px;
-    background-color: rgb(102, 102, 102);
-    border-radius: 20px;
+.Img {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url(../assets/Imagen/white-cubes-3d-background.jpg);
+  background-repeat: no-repeat;
+  width: 400px;
+  height: 500px;
+  border-radius: 20px;
+}
+
+.email {
+  background: transparent;
+  opacity: 0.1;
+}
+
+.password {
+  background: transparent;
+  opacity: 0.1;
+  margin: 0px 0px 0px 0px;
 }
 </style>
