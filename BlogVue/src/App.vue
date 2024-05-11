24921398d8b6347cb19d366cfc349d38ca27bb7b
QuispeJoseDavid <130864498+JoseDavidFull@users.noreply.github.com>
2024-05-11 07:12:58 -0600

    Actualizando Login version 0.0.2

--- BlogVue/src/App.vue
+++ BlogVue/src/App.vue
@@ -6,9 +6,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-    <router-link to="/login">Login</router-link>
     <router-link to="/etiqueta">Etiqueta</router-link>
-    
+    <router-link to="/login">Login</router-link>
   </div>
   <router-view></router-view>
 </template>
--- BlogVue/src/views/LoginView.vue
+++ BlogVue/src/views/LoginView.vue
@@ -3,39 +3,57 @@
 <template>
   <div class="Principal">
     <div class="Img">
-      <div class="email"><input type="email" placeholder="Email ID" /></div>
-      <div class="password">
-        <input type="password" placeholder="Password" />
-      </div>
+      <div class="email"><input type="email" placeholder="                    Email ID" /></div>
+      <div class="password"><input type="password" placeholder="                   Password" /></div>
+      <button class="BUTTON">LOGIN</button>
     </div>
   </div>
 </template>
-
 <style scoped>
 
-.Principal{
-    margin: 0px 0px 250px 400px;
+
+
+.Principal {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  height: 100vh; 
 }
 
 .Img {
-  display: flex;
-  align-items: center;
-  justify-content: center;
   background-image: url(../assets/Imagen/white-cubes-3d-background.jpg);
   background-repeat: no-repeat;
-  width: 400px;
+  width: 300px;
   height: 500px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center; 
+  box-shadow: 0px 40px 50px 10px; 
+}
+
+.email,
+.password {
+  margin-bottom: 20px; 
+}
+
+.email input,
+.password input {
+  width: 200px; 
+  height: 30px; 
+}
+
+.BUTTON {
+  width: 150px;
+  height: 30px;
+  border: 2px solid black;
   border-radius: 20px;
 }
 
-.email {
-  background: transparent;
-  opacity: 0.1;
+.BUTTON:hover {
+  background-color: rgba(75, 75, 75, 0.397);
 }
 
-.password {
-  background: transparent;
-  opacity: 0.1;
-  margin: 0px 0px 0px 0px;
-}
+
 </style>
