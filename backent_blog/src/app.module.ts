1d7c6f80a914ee7a27dc555a1e0c814f0e64d08c
beisagasaul <beisagasaul21@gmail.com>
2024-05-13 12:49:02 -0700

    configuracion de Data Base

--- backent_blog/src/app.module.ts
+++ backent_blog/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'usrblog',
+      password: '$aul10345806',
+      database: 'db_blog',
+      entities: [__dirname + '**/*.entity.ts'],
+      synchronize: true,
+      autoLoadEntities:true,
+    }),
+
   ],
   controllers: [AppController],
   providers: [AppService],
