8bac89a9eea0551b1c7ece21324a26cd8abb6996
beisagasaul <beisagasaul21@gmail.com>
2024-05-13 13:08:50 -0700

    creacion de usuario.entity

--- backent_blog/src/usuarios/entities/usuario.entity.ts
+++ backent_blog/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,27 @@
-export class Usuario {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('usuarios')
+export class Usuario {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column('varchar', { length: 45, nullable: false, name: 'nombre_usuario' })
+    nombreUsuario: string;
+
+    @Column('varchar', { length: 45, nullable: false })
+    clave: string;
+
+    @Column('varchar', { length: 50, nullable: false })
+    correo: string;
+
+    @Column('varchar', { length: 45, nullable: false })
+    rol: string;
+
+    @Column({ type: 'blob', nullable: true, name: 'imagen' })
+    imagen: Buffer;
+
+    @CreateDateColumn({ name: 'fecha_creacion' })
+    fechaCreacion: Date;
+
+    @UpdateDateColumn({ name: 'fecha_modificacion' })
+    fechaModificacion: Date;
+}
